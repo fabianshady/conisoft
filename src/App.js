6eb40c64ce834729edf5b1ddfe557c4c45985a10
EMMANUELRIVERA12 <90486130+EMMANUELRIVERA12@users.noreply.github.com>
2022-06-03 12:16:29 -0700

    add carrusel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Principal from './components/principal';
 import Slider from './components/slider';
 import Cards from './components/cards';
+import Carruselconteiner from './components/carruselconteiner';
 
 function App() {
   return (
     <div className="App">
       <Principal/>
       <Slider/>
+      <div className='carrusel'>
+        <Carruselconteiner/>
+      </div>
       <Cards/>
     </div>
   );
