2bcedb079395fe542bbb1f127a5e8deb0435e487
Fabian <fabianmendoza.py@gmail.com>
2022-06-03 18:17:12 -0700

    almost finish

--- src/App.js
+++ src/App.js
@@ -5,10 +5,66 @@ import Cards from './components/cards';
 
 function App() {
   return (
-    <div className="App">
-      <Principal/>
-      <Slider/>
-      <Cards/>
+    <div>
+      <nav class="menu-container">
+
+        <input type="checkbox" aria-label="Toggle menu" />
+        <span></span>
+        <span></span>
+        <span></span>
+
+
+
+        <div class="menu">
+          <ul>
+            <li>
+              <a href="#jiisic">
+                JIISIC'22
+              </a>
+            </li>
+            <li>
+              <a href="#topics">
+                TOPICS
+              </a>
+            </li>
+            <li>
+              <a href="#cfp">
+                CFP
+              </a>
+            </li>
+            <li>
+              <a href="#sub">
+                SUBMISSION
+              </a>
+            </li>
+            <li>
+              <a href="#regis">
+                REGISTRATION
+              </a>
+            </li>
+            <li>
+              <a href="#comm">
+                COMMITTEES
+              </a>
+            </li>
+            <li>
+              <a href="#ven">
+                VENUE
+              </a>
+            </li>
+            <li>
+              <a href="#past">
+                PAST EDITIONS
+              </a>
+            </li>
+          </ul>
+          <ul>
+          </ul>
+        </div>
+      </nav>
+      <Principal />
+      <Slider />
+      <Cards />
     </div>
   );
 }
